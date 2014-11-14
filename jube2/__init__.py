8569d9dba2b3ba32df1caa17098e977994e4dc41
thust1 <k.thust@fz-juelich.de>
2014-11-14 08:42:54 +0000

    added copyright notice (although not too useful, yet)

--- jube2/__init__.py
+++ jube2/__init__.py
@@ -1,1 +1,11 @@
 """jube2 package"""
+
+# #############################################################################
+# #  JUBE Benchmarking Environment                                           ##
+# #  http://www.fz-juelich.de/jsc/jube                                       ##
+# #############################################################################
+# #  Copyright (c) 2008-2014                                                 ##
+# #  Forschungszentrum Juelich, Juelich Supercomputing Centre                ##
+# #                                                                          ##
+# #  See the file LICENSE in the package base directory for details          ##
+# #############################################################################
