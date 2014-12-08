6e630559d782547a1a5715c1fcb76bfc93805f2a
thust1 <k.thust@fz-juelich.de>
2014-12-08 17:29:42 +0000

    splitted testing into single files

--- tests/parameter_tests.py
+++ tests/parameter_tests.py
@@ -16,7 +16,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""Testing routines"""
+"""Parameter related tests"""
 
 from __future__ import (print_function,
                         unicode_literals,
@@ -46,10 +46,12 @@ class TestParameter(unittest.TestCase):
         self.assertEqual(parameter.value, "2,3,4")
 
         # Template become constant check
+        outer_static_par = None
         for idx, static_par in enumerate(parameter.expand()):
             self.assertEqual(static_par.value, values[idx])
             self.assertFalse(static_par.is_template)
             self.assertTrue(static_par.is_equivalent(parameter))
+            outer_static_par = static_par
 
         # Copy check
         parameter2 = parameter.copy()
@@ -70,7 +72,7 @@ class TestParameter(unittest.TestCase):
         self.assertEqual(etree.get("type"), "string")
         self.assertEqual(etree.text, "2,3,4")
 
-        etree = static_par.etree_repr(use_current_selection=True)
+        etree = outer_static_par.etree_repr(use_current_selection=True)
         self.assertEqual(etree.text, "2,3,4")
         self.assertEqual(etree.get("selection"), "4")
 
@@ -78,6 +80,7 @@ class TestParameter(unittest.TestCase):
 class TestParameterSet(unittest.TestCase):
     """ParameterSet test class"""
     def test_set(self):
+        """Test ParameterSet functionality"""
         # Test add parameter to set
         parameterset = jube2.parameter.Parameterset("test")
         parameter = jube2.parameter.Parameter.create_parameter("test", "2")
@@ -155,8 +158,8 @@ class TestParameterSet(unittest.TestCase):
         parameterset.add_parameter(parameter2)
         self.assertTrue("test2" in parameterset.template_parameter_dict)
         param_list = ["2", "3", "4"]
-        for idx, new_parameterset in \
-            enumerate(parameterset.expand_templates()):
+        for idx, new_parameterset in (
+                enumerate(parameterset.expand_templates())):
             self.assertEqual(new_parameterset.template_parameter_dict, dict())
             self.assertFalse(new_parameterset.has_templates)
             self.assertEqual(new_parameterset["test2"].value, param_list[idx])
@@ -172,14 +175,14 @@ class TestParameterSet(unittest.TestCase):
         parameterset.add_parameter(parameter_eval)
         self.assertFalse(
             parameter_sub.can_substitute_and_evaluate(parameterset))
-        for idx, new_parameterset in \
-            enumerate(parameterset.expand_templates()):
+        for idx, new_parameterset in (
+                enumerate(parameterset.expand_templates())):
             self.assertTrue(
                 parameter_sub.can_substitute_and_evaluate(new_parameterset))
             new_parameterset.parameter_substitution(final_sub=True)
             self.assertEqual(new_parameterset["test4"].value, param_list[idx])
             self.assertEqual(new_parameterset["test5"].value,
-                              str(int(param_list[idx]) * 2))
+                             str(int(param_list[idx]) * 2))
 
         # Etree repr check
         etree = parameterset.etree_repr()
