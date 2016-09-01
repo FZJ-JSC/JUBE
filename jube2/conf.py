5ea57f8f86d180d8f3735c087b9047db9eddf839
luehrs2 <s.luehrs@fz-juelich.de>
2016-09-01 08:59:24 +0000

    prepare release 2.1.3

--- jube2/conf.py
+++ jube2/conf.py
@@ -22,7 +22,7 @@ from __future__ import (print_function,
                         division)
 
 # general
-JUBE_VERSION = "2.1.2"
+JUBE_VERSION = "2.1.3"
 ALLOWED_SCRIPTTYPES = ["python", "perl", "shell"]
 DEBUG_MODE = False
 VERBOSE_LEVEL = 0
