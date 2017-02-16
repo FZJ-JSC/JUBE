4172bbb4250db39bfa9a459921d688fe256f361f
luehrs2 <s.luehrs@fz-juelich.de>
2017-02-16 08:31:02 +0000

    update year

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,7 @@ else:
 
 # General information about the project.
 project = u'JUBE'
-copyright = u'2016, JUBE Developer Team, Forschungszentrum Jülich GmbH'
+copyright = u'2017, JUBE Developer Team, Forschungszentrum Jülich GmbH'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -218,7 +218,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index_user', 'JUBE.tex', u'JUBE Documentation',
-   u'2016, JUBE Developer Team,\\\\Forschungszentrum Jülich GmbH', 'manual'),
+   u'2017, JUBE Developer Team,\\\\Forschungszentrum Jülich GmbH', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -248,7 +248,7 @@ latex_logo = 'JUBE-Logo.png'
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'jube', u'JUBE Documentation',
-     [u'2016, JUBE Developer Team, Forschungszentrum Jülich GmbH'], 1)
+     [u'2017, JUBE Developer Team, Forschungszentrum Jülich GmbH'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -262,7 +262,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'JUBE', u'JUBE Documentation',
-   u'2016, JUBE Developer Team, Forschungszentrum Jülich GmbH', 
+   u'2017, JUBE Developer Team, Forschungszentrum Jülich GmbH', 
    'JUBE', 'One line description of project.',
    'Miscellaneous'),
 ]
@@ -281,9 +281,9 @@ texinfo_documents = [
 
 # Bibliographic Dublin Core info.
 epub_title = u'JUBE'
-epub_author = u'2016, JUBE Developer Team, Forschungszentrum Jülich GmbH'
-epub_publisher = u'2016, JUBE Developer Team, Forschungszentrum Jülich GmbH'
-epub_copyright = u'2016, JUBE Developer Team, Forschungszentrum Jülich GmbH'
+epub_author = u'2017, JUBE Developer Team, Forschungszentrum Jülich GmbH'
+epub_publisher = u'2017, JUBE Developer Team, Forschungszentrum Jülich GmbH'
+epub_copyright = u'2017, JUBE Developer Team, Forschungszentrum Jülich GmbH'
 
 # The language of the text. It defaults to the language option
 # or en if the language is not set.
