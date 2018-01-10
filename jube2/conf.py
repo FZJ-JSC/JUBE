88e14cd4b3d8437d2b2b20b260641d49546af1a5
luehrs2 <s.luehrs@fz-juelich.de>
2018-01-10 10:00:22 +0000

    update internal JUBE version number

--- jube2/conf.py
+++ jube2/conf.py
@@ -22,7 +22,7 @@ from __future__ import (print_function,
                         division)
 
 # general
-JUBE_VERSION = "2.1.4"
+JUBE_VERSION = "2.2.0"
 ALLOWED_SCRIPTTYPES = set(["python", "perl", "shell"])
 DEBUG_MODE = False
 VERBOSE_LEVEL = 0
