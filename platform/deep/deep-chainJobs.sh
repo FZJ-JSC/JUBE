81f0006a75bb9634ffcd77b0702f1d1a4cc1a035
luehrs2 <s.luehrs@fz-juelich.de>
2018-02-21 14:34:15 +0000

    update deep platform files

--- platform/deep/deep-chainJobs.sh
+++ platform/deep/deep-chainJobs.sh
@@ -14,9 +14,11 @@ SUBMITSCRIPT=$*
 if  [ -f $LOCKFILE ]
 then
     DEPEND_JOBID=`head -1 $LOCKFILE`
-    JOBID=`qsub -W depend=afterany:${DEPEND_JOBID} $SUBMITSCRIPT`
+    echo "sbatch --dependency=afterany:${DEPEND_JOBID} $SUBMITSCRIPT"
+    JOBID=`sbatch --dependency=afterany:${DEPEND_JOBID} $SUBMITSCRIPT`
 else
-    JOBID=`qsub $SUBMITSCRIPT`
+    echo "sbatch $SUBMITSCRIPT"
+    JOBID=`sbatch $SUBMITSCRIPT`
 fi
 
 JUBE_ERR_CODE=$?
@@ -24,7 +26,9 @@ if [ $JUBE_ERR_CODE -ne 0 ]; then
     exit $JUBE_ERR_CODE
 fi
 
-echo ${JOBID} > $LOCKFILE
+echo "RETURN: $JOBID"
+# the JOBID is the last field of the output line
+echo ${JOBID##* } > $LOCKFILE
 
 exit 0
 
