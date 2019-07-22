0d2d0ec9021158070e8e8bc778b848b656efb9b5
sluehrs <s.luehrs@fz-juelich.de>
2019-07-22 14:11:09 +0200

    add missing whitespaces

--- jube2/main.py
+++ jube2/main.py
@@ -505,7 +505,7 @@ def _benchmark_result(benchmark_folder, args, result_list=None):
 
     # Create benchmark results
     result_list = benchmark.create_result(only=args.only,
-                                          data_list=result_list, 
+                                          data_list=result_list,
                                           style=args.style)
 
     # Reset logging
@@ -724,8 +724,8 @@ def gen_subparser_conf():
             ("-n", "--num"):
                 {"type": int, "help": "show only last N benchmarks"},
             ("-s", "--style"):
-                {"help": "sets style for table", 
-                 "choices":["pretty", "csv"]}
+                {"help": "sets style for table",
+                 "choices": ["pretty", "csv"]}
         }
     }
 
