bef59854ae336fe7a00bd18183aff46490929c7b
wellmann2 <j.wellmann@fz-juelich.de>
2020-10-29 09:16:31 +0100

    added try-except block without testing

--- jube2/analyser.py
+++ jube2/analyser.py
@@ -441,10 +441,14 @@ class Analyser(object):
                             match_dict[pattern.name]["sum"] += match
                         else:
                             match_dict[pattern.name]["sum"] = match
-                        if "sum2" in match_dict[pattern.name]:
-                            match_dict[pattern.name]["sum2"] += match ** 2
-                        else:
-                            match_dict[pattern.name]["sum2"] = match ** 2
+                        try:
+		                    if "sum2" in match_dict[pattern.name]:
+		                        match_dict[pattern.name]["sum2"] += match ** 2
+		                    else:
+		                        match_dict[pattern.name]["sum2"] = match ** 2
+		                except OverflowError:
+		                	LOGGER.warning(("Sum2 cannot be represented, numerical result out of range")
+		                	match_dict[pattern.name]["sum2"] = math.nan
 
                     if "cnt" in match_dict[pattern.name]:
                         match_dict[pattern.name]["cnt"] += 1
@@ -458,11 +462,14 @@ class Analyser(object):
                              match_dict[pattern.name]["cnt"])
 
                     if match_dict[pattern.name]["cnt"] > 1:
-                        match_dict[pattern.name]["std"] = math.sqrt(
-                            (abs(match_dict[pattern.name]["sum2"] -
-                                 (match_dict[pattern.name]["sum"] ** 2 /
-                                  match_dict[pattern.name]["cnt"])) /
-                             (match_dict[pattern.name]["cnt"] - 1)))
+                    	try:
+		                    match_dict[pattern.name]["std"] = math.sqrt(
+		                        (abs(match_dict[pattern.name]["sum2"] -
+		                             (match_dict[pattern.name]["sum"] ** 2 /
+		                              match_dict[pattern.name]["cnt"])) /
+		                         (match_dict[pattern.name]["cnt"] - 1)))
+		                except OverflowError:
+		                	match_dict[pattern.name]["std"] = 0
                     else:
                         match_dict[pattern.name]["std"] = 0
 
