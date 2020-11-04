31e6648827c0836c3cba77bb94860afa5524568b
j.wellmann <j.wellmann@fz-juelich.de>
2020-11-04 09:38:14 +0100

    taberror

--- jube2/analyser.py
+++ jube2/analyser.py
@@ -442,13 +442,13 @@ class Analyser(object):
                         else:
                             match_dict[pattern.name]["sum"] = match
                         try:
-		                    if "sum2" in match_dict[pattern.name]:
-		                        match_dict[pattern.name]["sum2"] += match ** 2
-		                    else:
-		                        match_dict[pattern.name]["sum2"] = match ** 2
-		                except OverflowError:
-		                	LOGGER.warning(("Sum2 cannot be represented, numerical result out of range")
-		                	match_dict[pattern.name]["sum2"] = math.nan
+                            if "sum2" in match_dict[pattern.name]:
+                                match_dict[pattern.name]["sum2"] += match ** 2
+                            else:
+                                match_dict[pattern.name]["sum2"] = match ** 2
+                        except OverflowError:
+                            LOGGER.warning("Sum2 cannot be represented, numerical result out of range")
+                            match_dict[pattern.name]["sum2"] = math.nan
 
                     if "cnt" in match_dict[pattern.name]:
                         match_dict[pattern.name]["cnt"] += 1
@@ -464,12 +464,12 @@ class Analyser(object):
                     if match_dict[pattern.name]["cnt"] > 1:
                     	try:
 		                    match_dict[pattern.name]["std"] = math.sqrt(
-		                        (abs(match_dict[pattern.name]["sum2"] -
-		                             (match_dict[pattern.name]["sum"] ** 2 /
+		                              (abs(match_dict[pattern.name]["sum2"] - 
+		                              (match_dict[pattern.name]["sum"] ** 2 /
 		                              match_dict[pattern.name]["cnt"])) /
-		                         (match_dict[pattern.name]["cnt"] - 1)))
-		                except OverflowError:
-		                	match_dict[pattern.name]["std"] = 0
+		                              (match_dict[pattern.name]["cnt"] - 1)))
+                        except OverflowError:
+                            match_dict[pattern.name]["std"] = 0
                     else:
                         match_dict[pattern.name]["std"] = 0
 
