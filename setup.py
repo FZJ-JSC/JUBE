bece329a6c8c944dd5c8c8851d9693fee4bc5d99
Andreas Smolenko <smolenko1@jwlogin02.juwels>
2022-07-07 10:16:29 +0200

    adapt version number in setup.py

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ config = {'name': 'JUBE',
               "License :: OSI Approved :: GNU General Public License v3 " +
               "(GPLv3)",
               "Operating System :: POSIX :: Linux",
-              "Programming Language :: Python :: 2.6",
+              "Programming Language :: Python :: 3.2",
               "Topic :: System :: Monitoring",
               "Topic :: System :: Benchmark",
               "Topic :: Software Development :: Testing"],
